bc52bd6c4184fe0bf289c5a5b3505af71b9a87cc
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-03-13 22:37:55 -0600

    Update functions_ej2.py

--- functions_ej2.py
+++ functions_ej2.py
@@ -25,15 +25,15 @@ def total_distance(ciudades_list):
 
 
 def sim_annealing(ciudades_list, numero_ciudades, ciudad_inicial):
-    before = ciudades_list.copy()
-    cost0 = total_distance(ciudades_list)
-    T = 30
+    firstList = ciudades_list.copy()
+    initialCost = total_distance(ciudades_list)
+    times = 30
     factor = 0.99
-    T_init = T
+    T_init = times
     costo = 0
     for _ in range(1000):
-        T = T * factor
-        for j in range(500):
+        times *= factor
+        for _ in range(500):
             r1, r2 = np.random.randint(0, numero_ciudades, size=2)
             r1 = r1+1
             r2 = r2+1
@@ -42,36 +42,48 @@ def sim_annealing(ciudades_list, numero_ciudades, ciudad_inicial):
             ciudades_list[f'ciudad {str(r2)}'] = temp
             cost1 = total_distance(ciudades_list)
 
-            if cost1 < cost0:
-                cost0 = cost1
+            if cost1 < initialCost:
+                initialCost = cost1
             else:
                 x = np.random.uniform()
-                if x < np.exp((cost0-cost1)/T):
-                    cost0 = cost1
+                if x < np.exp((initialCost-cost1)/times):
+                    initialCost = cost1
                 else:
                     temp = ciudades_list[f'ciudad {str(r1)}']
                     ciudades_list['ciudad ' +
                                   str(r1)] = ciudades_list[f'ciudad {str(r2)}']
                     ciudades_list[f'ciudad {str(r2)}'] = temp
-    costo = cost0
-    after = ciudades_list
+    costo = initialCost
+    newList = ciudades_list
     recorrido = []
 
     for i in range(len(list(ciudades_list))):
-        if after['ciudad '+str(i+1)] == ciudad_inicial:
-            for key, value in before.items():
-                if value == after['ciudad '+str(i+1)]:
-                    recorrido.append(key)
+        if newList[f'ciudad {str(i+1)}'] == ciudad_inicial:
+            recorrido.extend(
+                key
+                for key, value in firstList.items()
+                if value == newList[f'ciudad {str(i+1)}']
+            )
+
             i = i+1
             for j in range(len(list(ciudades_list))):
 
                 j = j+1
                 if (i+j) % len(list(ciudades_list)) == 0:
-                    for key, value in before.items():
-                        if value == after['ciudad '+str(len(list(ciudades_list)))]:
-                            recorrido.append(key)
+                    recorrido.extend(
+                        key
+                        for key, value in firstList.items()
+                        if value == newList[f'ciudad {len(list(ciudades_list))}']
+                    )
+
                 else:
-                    for key, value in before.items():
-                        if value == after['ciudad '+str((i+j) % len(list(ciudades_list)))]:
-                            recorrido.append(key)
+                    recorrido.extend(
+                        key
+                        for key, value in firstList.items()
+                        if value
+                        == newList[
+                            'ciudad ' + str((i + j) % len(list(ciudades_list)))
+                        ]
+                    )
+
     return costo, ciudades_list, recorrido
