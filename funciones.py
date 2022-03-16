7fb5c06087cbab077a212ca0da6f52dfe7f09207
Donaldo Sebastian Garcia <gar19683@uvg.edu.gt>
2022-03-15 23:02:48 -0600

    feat: funciones

--- funciones.py
+++ funciones.py
@@ -2,16 +2,16 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 def funcion_a(param):
-  return (param[0]**4) + (param[1]**4) - (4*param[0]*param[1]) + (0.5*param[1]) + 1
+  return (param[0]*param[0]*param[0]*param[0]) + (param[1]**4) - (4*param[0]*param[1]) + (0.5*param[1]) + 1
 
 def derivada_a(param):
-  return (4*(param[0]**3) - 4*param[1])*np.array([1., 0.]) + (4*(param[1]**3) - 4*param[0] + 0.5)*np.array([0., 1.])
+  return (4*(param[0]*param[0]*param[0]) - 4*param[1])*np.array([1., 0.]) + (4*(param[1]*param[1]*param[1]) - 4*param[0] + 0.5)*np.array([0., 1.])
 
 def funcion_b(param):
   return 100*(param[1] - param[0]**2)**2 + (1 - param[0])**2
 
 def derivada_b(param):
-  return (400*param[0]**3 - 400*param[0]*param[1] + 2*param[0] - 2)*np.array([1., 0.]) + (200*(param[1] - param[0]**2))*np.array([0., 1.])
+  return (400 * param[0]*param[0]*param[0] - 400 * param[0] * param[1] + 2 * param[0] - 2) * np.array([1., 0.]) + (200 *( param[1] - param[0]*param[0])) * np.array([0., 1.])
 
 def funcion_c(param):
   count = 0
@@ -27,9 +27,9 @@ def derivada_c(param):
 
   for i in range(1, 9):
     x, y, z = param[i-1], param[i], param[i+1]
-    di = 200*(z - y**2)*(-2*y) - 2*(1 - y) + 200*(y - x**2)
+    di = 200*(z - y*y)*(-2*y) - 2*(1 - y) + 200*(y - x*x)
     lista.append(di)
-  lista.append(200*(param[9] - param[8]**2))
+  lista.append(200*(param[9] - param[8]*param[8]))
 
   return np.array(lista)
 
